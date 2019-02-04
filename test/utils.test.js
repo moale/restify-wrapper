1a1d029c43c23c605091411a495070b147ccbad2
alex m <alexmorichetti@gmail.com>
2019-02-04 22:53:07 +0100

    start

---
+++ test/utils.test.js
@@ -0,0 +1,32 @@
+const { enhance } = require('./../lib/utils');
+
+describe('#enhance', () => {
+  it('should add to instance the right key => value', () => {
+    const foo = {};
+
+    enhance(foo, 'bar', () => 42);
+
+    expect(foo).toHaveProperty('bar');
+    expect(typeof foo.bar).toBe('function');
+  });
+
+  it('should add an obj with setter and getter', () => {
+    const foo = {};
+
+    enhance(foo, 'bar', {
+      setter(value) {
+        this._myPrivateVar = value;
+      },
+
+      getter() {
+        return this._myPrivateVar;
+      },
+    });
+
+    expect(foo).toHaveProperty('bar');
+    expect(foo.bar).toBeUndefined();
+
+    foo.bar = 42;
+    expect(foo.bar).toBe(42);
+  });
+});
